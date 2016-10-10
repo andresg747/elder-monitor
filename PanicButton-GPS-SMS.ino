cebb9f501fb87067e41ae9dcb3f2cc429d8d6e61
Andrés García <andresg747@gmail.com>
2016-10-10 01:50:34 -0400

    Mejorando Botón de Pánico
    Creadas rutinas que se ejecutarán cada 15 seg y 60 seg respectivamente.
    Para actualizar de manera constante Localización, Batería y
    (posteriormente) BPM.

--- PanicButton-GPS-SMS.ino
+++ PanicButton-GPS-SMS.ino
@@ -10,14 +10,18 @@
 // LED pin
 const int ledPin = 6;
 int buttonPin = 7;
-int current;         // Current state of the button
-                     // (LOW is pressed b/c i'm using the pullup resistors)
-long millis_held;    // How long the button was held (milliseconds)
-long secs_held;      // How long the button was held (seconds)
-long prev_secs_held; // How long the button was held in the previous check
+int current; // Activo en bajo.
+long millis_held;
+long secs_held;
+long prev_secs_held;
 byte previous = HIGH;
-unsigned long firstTime; // how long since the button was first pressed 
-int flag=0;
+unsigned long firstTime;
+int SOS_flag=0; // Por si se intentó enviar SOS y aún no se tiene GPSFIX o nó se pudo enviar.
+char sendto[]="04245328127";
+uint16_t vbat;
+
+#define quinceSeg (1000UL * 15) // Base rutina c/15 Seg
+#define unMin (1000UL * 60)  // Base rutina c/1 min
 
 // Latitude & longitude for distance measurement
 float latitude, longitude, speed_kph, heading, altitude;
@@ -52,13 +56,6 @@ void setup() {
 
   Serial.println(F("FONA Conectado!"));
 
-    // Print module IMEI number.
-  char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
-  uint8_t imeiLen = fona.getIMEI(imei);
-  if (imeiLen > 0) {
-    Serial.print("Module IMEI: "); Serial.println(imei);
-  }
-
   // Use the watchdog to simplify retry logic and make things more robust.
   // Enable this after FONA is intialized because FONA init takes about 8-9 seconds.
   //Watchdog.enable(8000);
@@ -76,9 +73,27 @@ void loop() {
 
   checkbutton();
 
-  // Wait 5 secs
-  delay(100);
+  static unsigned long aux60 = 0 - unMin;
+  static unsigned long aux15 = 0 - quinceSeg;
 
+
+  if ((long)(millis() - aux60) >= unMin){
+    aux60 += unMin;
+    // Cada 60 seg
+    checkGPS();
+    if (! fona.getBattPercent(&vbat)) {
+      Serial.println(F("No pudo obtener nivel de Bateria"));
+    } else {
+      Serial.print(F("Porcentaje Bateria = ")); Serial.print(vbat); Serial.println(F("%"));
+    }
+  }else if((long)(millis() - aux15) >= quinceSeg){
+    aux15 += quinceSeg;
+    // Cada 15 seg
+    if(SOS_flag == 1){
+      sendSMS_SOS();
+    }
+  }
+  delay(10);
 }
 
 void checkbutton(){
@@ -91,30 +106,19 @@ void checkbutton(){
 
   millis_held = (millis() - firstTime);
   secs_held = millis_held / 1000;
-
+  // Chequea si el botón realmente fue pulsado (debounce)
   if (millis_held > 50) {
-
+    // Pulsado por más de 2800ms
     if (current == LOW && secs_held > prev_secs_held ) {
-      if (millis_held > 2800 )
+      if (millis_held > 2900 )
       {
         digitalWrite(ledPin,HIGH);
       }
     }
-
-    // check if the button was released since we last checked
     if (current == HIGH && previous == LOW) {
-      // HERE YOU WOULD ADD VARIOUS ACTIONS AND TIMES FOR YOUR OWN CODE
-      // ===============================================================================
-
-      // Button held for 1-3 seconds, print out some info
-      if (secs_held >= 3) {
-        Serial.print("Seconds held: ");
-        Serial.print(secs_held);
-        Serial.print("  Milliseconds held: ");
-        Serial.println(millis_held);
-        readGPS();
+      if (millis_held >= 3000) {
+        sendSMS_SOS();
       }
-      // ===============================================================================
     }
   }
 
@@ -125,56 +129,65 @@ void checkbutton(){
     digitalWrite(ledPin,LOW);
 }
 
-void ledblink(int times, int lengthms, int pinnum){
-  for (int x=0; x<times;x++) {
-    digitalWrite(pinnum, HIGH);
-    delay (lengthms);
-    digitalWrite(pinnum, LOW);
-    delay(lengthms);
-  }
-}
-
-void readGPS(){
-  // Grab a GPS reading.
-  float latitude, longitude, speed_kph, heading, altitude;
+void sendSMS_SOS(){
+  // Lectura GPS
   bool gpsFix = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
 
   if(gpsFix){
-    Serial.print("GPS_FIX: ");
-    Serial.print(gpsFix);
-    Serial.println("");
-
-    Serial.print("Latitude: ");
+    Serial.print("Latitud: ");
     printFloat(latitude, 5);
     Serial.println("");
 
-    Serial.print("Longitude: ");
+    Serial.print("Longitud: ");
     printFloat(longitude, 5);
     Serial.println("");
-    flag=1;
-    sendSMS(latitude,longitude);
-  }else if (!gpsFix){
-    Serial.print("GPS No Disponible");
-    flag=0;
-  }
-}
 
-void sendSMS(float latitude, float longitude){
-  if(flag){
-    char sendto[]="04245328127";
-    flag=0;
+    // Concateno mensaje con Latitud y Longitud
     char mensaje_txt[140];
     strcpy(mensaje_txt, "SOS! Ubicacion: https://maps.google.com/?q=");
     dtostrf(latitude,6,5,&mensaje_txt[strlen(mensaje_txt)]);
     strcat(mensaje_txt,",");
     dtostrf(longitude,6,5,&mensaje_txt[strlen(mensaje_txt)]);
     strcat(mensaje_txt," BPM: XX");
+
+    // Imprimo mensaje en serial
     Serial.println(mensaje_txt);
+
+    //Envío mensaje
     if (!fona.sendSMS(sendto, mensaje_txt)) {
-      Serial.println(F("Failed"));
+      Serial.println(F("No se pudo enviar SMS"));
+      SOS_flag = 1;
     } else {
-      Serial.println(F("Sent!"));
+      Serial.println(F("Mensaje enviado con exito!"));
+      SOS_flag = 0;
     }
+    // Actualiza IoT
+  }else if (!gpsFix){
+    SOS_flag = 1;
+    Serial.println(F("Aun no hay GPSFIX, se reintentara de nuevo. "));
+    char mensaje_txt[140];
+    strcpy(mensaje_txt, "SOS! Sin señal GPS. Ultima ubicacion: https://io.adafruit.com/andresg747/ BPM: XX Se notificara de inmediato cuando GPS este disponible.");
+    if (!fona.sendSMS(sendto, mensaje_txt)) {
+      Serial.println(F("No se pudo enviar SMS"));
+    } else {
+      Serial.println(F("Mensaje enviado con exito!"));
+    }
+  }
+}
+
+void checkGPS(){
+  bool gpsFix = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
+
+  if(gpsFix){
+    Serial.print("Latitud: ");
+    printFloat(latitude, 5);
+    Serial.println("");
+
+    Serial.print("Longitud: ");
+    printFloat(longitude, 5);
+    Serial.println("");
+  }else{
+    Serial.print("GPS No disponible.");
   }
 }
 
@@ -253,7 +266,3 @@ void printFloat(float value, int places) {
   }
 }
 
-void flushSerial() {
-  while (Serial.available())
-    Serial.read();
-}
